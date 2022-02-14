0fa08940f798bb1fd5f6c75279d20ce7192011d3
nika-karsanova <vero.karsanova@gmail.com>
2022-02-14 17:36:41 +0000

    Open CV tutorial, added new directories

---
+++ src/spiking/main.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+pathdir = "C:/Users/welleron/Desktop/mmp/datasets/womens_sp/videos/"
+outpath = "C:/Users/welleron/Desktop/mmp/tutorials/output/"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
